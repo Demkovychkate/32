f33e2916152a2fe4f43889bbd776c37b8bd69a85
Demkovychkate <Panchevakate1804@gmail.com>
2023-11-10 23:18:50 +0200

    Task

--- script.js
+++ script.js
@@ -1,2 +1,37 @@
+const btn = document.querySelector('#submit');
+const firstNumber = document.querySelector('.firstNumber');
+const znak = document.querySelector('.znak');
+const secondNumber = document.querySelector('.secondNumber');
+const result = document.querySelector('.result');
 
-const btn = document.querySelector('.btn');
+class SuperMath {
+   
+    operations = {
+        '+': (X, Y) => X + Y,
+        '-': (X, Y) => X - Y,
+        '/': (X, Y) => X / Y,
+        '*': (X, Y) => X * Y,
+    };
+
+    input() {
+        this.x = Number(firstNumber.value);
+        this.znak = znak.value;
+        this.y = Number(secondNumber.value);
+    }
+
+    check() {
+        const resultValue = this.operations[this.znak](this.x, this.y);
+        result.value = resultValue;
+    }
+
+    getOperation() {
+        return Object.keys(this.operations).join(', ');
+    }
+}
+
+const superMath = new SuperMath();
+
+btn.addEventListener('click', function () {
+    superMath.input();
+    superMath.check();
+});
